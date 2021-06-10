ef07c7550b7f35fbfd863ece047cbdfb04332536
VarvadiukCursor <dianavarvadiuk@gmail.com>
2021-06-10 11:13:13 +0300

    change some problems style cod

---
+++ HW1/variables-and-types.js
@@ -0,0 +1,36 @@
+const plate = 15.678;
+const kettle = 123.965;
+const cup = 90.2345;
+
+const sumgoods = plate + kettle + cup;
+let floorplate = null;
+let floorkettle = null;
+let floorcup = null;
+const paymoney = 500;
+const quantitygoods = 3 ;
+const maxPrice = Math.max(plate,kettle,cup);
+console.log(`maxPrice: ${maxPrice}`);
+const minPrice = Math.min(plate,kettle,cup);
+
+console.log(`minPrice : ${minPrice}`);
+console.log(`sumgoods : ${sumgoods}`);
+console.log(floorplate = Math.floor(plate));
+console.log(floorkettle = Math.floor(kettle));
+console.log(floorcup = Math.floor(cup));
+const sumfloorGoods = floorplate + floorkettle + floorcup;
+console.log(`sumfloorGoods : ${sumfloorGoods}`);
+console.log(`sumfloorGoods rounded to hundreds : ${Math.round(sumfloorGoods / 100) *100}`);
+const isEvent = (sumfloorGoods % 2 !== 0);
+console.log(`isEvent : ${isEvent}`);
+const remainder = paymoney - sumgoods;
+console.log(`remainder : ${remainder}`);
+const averagevalue = +((sumgoods / quantitygoods).toFixed(2));
+console.log(`averagevalue : ${averagevalue}`);
+
+
+const discount = Math.round(Math.random() *  100);
+console.log(`discount : ${discount}`);
+const priceDiscount = +(sumfloorGoods - (sumfloorGoods*(discount / 100))).toFixed(2);
+console.log(`priceDiscount : ${priceDiscount}`);
+const netProfit = +(priceDiscount - (sumfloorGoods / 2)).toFixed(2);
+console.log(`netProfit : ${netProfit}`);
